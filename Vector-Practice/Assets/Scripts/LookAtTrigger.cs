cb987201f49915621292dea1b2477d76e51b0a69
doodlepickle <hidoodlepickle@gmail.com>
2021-11-05 07:30:12 +0000

    5 Nov - Implemented LookAtTrigger
    Implemented a LookAtTrigger for reference, began the implementation of a SpaceTransformation Function

--- Vector-Practice/Assets/Scripts/LookAtTrigger.cs
+++ Vector-Practice/Assets/Scripts/LookAtTrigger.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class LookAtTrigger : MonoBehaviour
 {
-    
+    [Range(0f, 1f)]
+    public float strictness = 0.5f;
+
+    public Transform playerTF;
+
+    private void OnDrawGizmos()
+    {
+
+        Vector2 position = transform.position;
+        Vector2 playerPos = playerTF.transform.position;
+        Vector2 playerDirection = playerTF.right;
+
+        Vector2 playerToTriggerDir = (position - playerPos).normalized;
+
+
+
+        //I need to take the DOT between the playerPos and the enemy position, normalizing both
+
+        float lookness = Vector2.Dot(playerToTriggerDir, playerDirection);
+
+        bool canSee = lookness < strictness;
+
+        Gizmos.color = canSee ? Color.red : Color.green;
+        Gizmos.DrawLine(playerPos, playerPos + playerToTriggerDir);
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(playerPos, playerPos + playerDirection);
+    }
 }
---
+++ Vector-Practice/Assets/Scripts/SpaceTransform.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceTransform : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        //Visual the basis vectors for the local object
+
+    }
+}
