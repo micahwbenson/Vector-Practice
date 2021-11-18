9111ee0e42d83bda931a9aeb3e664b021daf9303
doodlepickle <91175842+doodlepickle@users.noreply.github.com>
2021-11-18 08:40:45 +0000

    18 Nov - Implemented Adaptable Field of View Camera

--- Vector-Practice/Assets/Scripts/FoVScript.cs
+++ Vector-Practice/Assets/Scripts/FoVScript.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class FoVScript : MonoBehaviour
 {
-    public Transform obj;
+    public Transform[] obj;
+
     private void OnDrawGizmos()
     {
         //Oh, wow it does work . . . that is interesting!!!
         Camera cam = GetComponent<Camera>();
 
-        Vector2 relObjPos = obj.position - cam.transform.position; // Gives you the specific location of the object to work off - relative to the camera, right?
-
-        float opposite = relObjPos.y;
-        float adjacent = relObjPos.x; //These are coordinate measures - I guess because you are working in the local space of the camera . . .
-
-        // tan(angle) = o/a; -- need to reset this with algebra
-        // angle = atan (o/a);
-
-        //Apply this to the camera because it's not using it
-        float angRad = Mathf.Atan(opposite / adjacent);
-
         //FoV on Camera is in degrees - multiple angle by two to actually get the full angle value
-        cam.fieldOfView = 2 * angRad * Mathf.Rad2Deg;
+        float camAngle = MaxAngle();
+        cam.fieldOfView = 2 * camAngle * Mathf.Rad2Deg;
+    }
+
+    float MaxAngle()
+    {
+        float minAngle;
+        float maxAngle = 0;
+        for (int i = 0; i < obj.Length; i++)
+        {
+            Vector2 relObjPos = obj[i].position; // Gives you the specific location of the object to work off - relative to the camera, right?
+
+            float opposite = relObjPos.y;
+            float adjacent = relObjPos.x; //These are coordinate measures - I guess because you are working in the local space of the camera . . .
+
+            // tan(angle) = o/a; -- need to reset this with algebra
+            // angle = atan (o/a);
+
+            //Apply this to the camera because it's not using it
+            float angle = Mathf.Atan(opposite / adjacent);
+            minAngle = angle;
+
+            if (minAngle > maxAngle)
+            {
+                maxAngle = angle;
+            }
+        }
+        return maxAngle;
     }
 }
+
